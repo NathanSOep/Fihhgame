87889d5d40b3c2aa70c61af23d11e1cb18b5430e
NathanSoep <nathanvisser06@outlook.com>
2026-05-11 15:50:57 +0200

    initial commit

--- src/js/game.js
+++ src/js/game.js
@@ -1,9 +1,9 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
+import { Actor, Engine, Vector, DisplayMode, Resource } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 
 export class Game extends Engine {
-
+    
     constructor() {
         super({ 
             width: 1280,
@@ -16,16 +16,34 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+    
+        for (let index = 0; index < 100; index++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.Fish.toSprite())
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+            fish.vel = new Vector(Math.random() * -100, Math.random()*100)
+            this.add(fish)
+        }
+
+        for (let index = 0; index < 5; index++) {
+            this.addActor(Resources.Shark.toSprite(), 1000, 10j);
+        }
+
+        
     }
+    addActor(sprite, xRange, yRange) {
+        const actor = new Actor();
+        actor.graphics.use(sprite)
+        actor.events.on("exitviewport", (e)=> this.fishLeft(e))
+        actor.pos = new Vector(Math.random() * 1280, Math.random() * 720)
+        actor.vel = new Vector(Math.random() * xRange, Math.random()* yRange)
+        this.add(actor)    
+    }
+    
 
     fishLeft(e) {
-        e.target.pos = new Vector(1350, 300)
+        e.target.pos = new Vector(Math.random()* 1350,Math.random()* 300)
     }
 }
 
