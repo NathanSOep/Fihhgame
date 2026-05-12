7518f0c854a8e6d0faa4085f768202e632081d5e
NathanSoep <nathanvisser06@outlook.com>
2026-05-12 09:51:42 +0200

    Added bubbles

--- src/js/game.js
+++ src/js/game.js
@@ -16,34 +16,33 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
+        for (let index = 0; index < 150; index++) {
+            this.addActor(Resources.Blub.toSprite(), 10, -50)
+            
+        }
     
         for (let index = 0; index < 100; index++) {
-            const fish = new Actor()
-            fish.graphics.use(Resources.Fish.toSprite())
-            fish.events.on("exitviewport", (e) => this.fishLeft(e))
-            fish.pos = new Vector(Math.random() * 1280, Math.random() * 720)
-            fish.vel = new Vector(Math.random() * -100, Math.random()*100)
-            this.add(fish)
+            this.addActor(Resources.Fish.toSprite(), -100, 10)
         }
 
         for (let index = 0; index < 5; index++) {
-            this.addActor(Resources.Shark.toSprite(), 1000, 10);
+            this.addActor(Resources.Shark.toSprite(), 1000, 100);
         }
 
         
     }
-    addActor(sprite, xRange, yRange) {
+    addActor(sprite, xV, yV) {
         const actor = new Actor();
         actor.graphics.use(sprite)
         actor.events.on("exitviewport", (e)=> this.fishLeft(e))
         actor.pos = new Vector(Math.random() * 1280, Math.random() * 720)
-        actor.vel = new Vector(Math.random() * xRange, Math.random()* yRange)
+        actor.vel = new Vector(Math.random() * xV, Math.random()* yV)
         this.add(actor)    
     }
     
 
     fishLeft(e) {
-        e.target.pos = new Vector(Math.random()* 1350,Math.random()* 300)
+        e.target.pos = new Vector(Math.random()* 1280,Math.random()* 720)
     }
 }
 
